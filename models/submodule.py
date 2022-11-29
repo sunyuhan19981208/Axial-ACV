593b6e899a83e7a9ef38688002b3598d02a376ff
sunyuhan <sunyuhan@zju.edu.cn>
2022-11-29 10:17:07 +0800

    feat: mutihead

--- models/submodule.py
+++ models/submodule.py
@@ -398,6 +398,9 @@ class row_attention_block(nn.Module):
         self.value_conv = nn.Conv3d(in_channels=in_dim, out_channels = self.in_dim, kernel_size=1)
         self.softmax = nn.Softmax(dim=2)
         self.gamma = nn.Parameter(torch.zeros(1))
+        self.num_heads = 8
+        head_dim = self.in_dim // self.num_heads
+        self.scale = head_dim ** -0.5
     def forward(self, x):
         '''
         Parameters
@@ -412,14 +415,14 @@ class row_attention_block(nn.Module):
         Q = self.query_conv(x) #size = (b,c2, h,w)
         K = self.key_conv(x)   #size = (b, c2, h, w)
         V = self.value_conv(x) #size = (b, c1,h,w)
-        
-        Q = Q.permute(0,2,3,1,4).contiguous().view(B*D*H, -1,W).permute(0,2,1) #size = (b*h,w,c2)
-        K = K.permute(0,2,3,1,4).contiguous().view(B*D*H, -1,W)  #size = (b*h,c2,w)
-        V = V.permute(0,2,3,1,4).contiguous().view(B*D*H, -1,W)  #size = (b*h, c1,w)
+        hn = self.num_heads
+        Q = Q.permute(0,2,3,1,4).contiguous().view(B*D*H, hn, -1, W).permute(0,1,3,2) #size = (b*h*d,hn,w,c2)
+        K = K.permute(0,2,3,1,4).contiguous().view(B*D*H, hn, -1, W)  #size = (b*h,c2,w)
+        V = V.permute(0,2,3,1,4).contiguous().view(B*D*H, hn, -1, W)  #size = (b*h, c1,w)
         
         #size = (b*h,w,w) [:,i,j] 表示Q的所有h的第 Wi行位置上所有通道值与 K的所有h的第 Wj列位置上的所有通道值的乘积，
         # 即(1,c2) * (c2,1) = (1,1)
-        row_attn = torch.bmm(Q,K) 
+        row_attn = (Q @ K) * self.scale # B*D*H, hn, W, W
         ########
         #此时的 row_atten的[:,i,0:w] 表示Q的所有h的第 Wi行位置上所有通道值与 K的所有行的 所有列(0:w)的逐个位置上的所有通道值的乘积
         #此操作即为 Q的某个（i,j）与 K的（i,0:w）逐个位置的值的乘积，得到行attn
@@ -428,7 +431,7 @@ class row_attention_block(nn.Module):
         row_attn = self.softmax(row_attn) #对列进行softmax，即[k,i,0:w] ，某一行的所有列加起来等于1，
         #size = (b*h,c1,w) 这里先需要对row_atten进行 行列置换，使得某一列的所有行加起来等于1
         #[:,i,j]即为V的所有行的某个通道上，所有列的值 与 row_attn的行的乘积，即求权重和
-        out = torch.bmm(V,row_attn.permute(0,2,1)) 
+        out = V @ row_attn.permute(0,1,3,2) # B*D*H, hn, 
         #size = (b,c1,h,2)
         out = out.view(B, D, H, C, W).permute(0,3,1,2,4)
         out = self.gamma*out + x 
